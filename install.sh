873370cea39b6011203a24dc3d0311422ca220ff
jotamontecino <jotamontecino@gmail.com>
2024-01-07 22:01:47 +0100

    WIP

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ VERSION=0.0.1
 ## Download github
 echo "Downloading taskfiles"
 wget "https://github.com/jotamontecino/env_scripts/archive/refs/tags/v$VERSION.tar.gz" -O /tmp/taskfile.tar.gz
-tar -zxvf /tmp/taskfile.tar.gz -C /tmp/
+tar -zxf /tmp/taskfile.tar.gz -C /tmp/
 rm /tmp/taskfile.tar.gz
 mkdir $HOME/.taskfiles
 echo "Create Home folder"
