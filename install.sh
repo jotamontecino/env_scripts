e0ab5f37c588f6bcb58f5a62610f78664b8ba07d
jotamontecino <jotamontecino@gmail.com>
2024-01-07 21:50:49 +0100

    WIP

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@ VERSION=v0.0.1
 
 ## Download github
 echo "Downloading taskfiles"
-echo "https://github.com/jotamontecino/env_scripts/archive/refs/tags/$VERSION.tar.gz"
-wget "https://github.com/jotamontecino/env_scripts/archive/refs/tags/$VERSION.tar.gz" -O /tmp/taskfile.tar.gz
+wget "https://github.com/jotamontecino/env_scripts/archive/refs/tags/v0.0.1.tar.gz" -O /tmp/taskfile.tar.gz
 tar -zxvf /tmp/taskfile.tar.gz -C /tmp/
 
 ## Install taskfile
