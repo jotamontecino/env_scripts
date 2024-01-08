4ad2993ce2b2b47a2c92f9424fdf4546e5f4347b
jotamontecino <jotamontecino@gmail.com>
2024-01-08 12:12:25 +0100

    WIP

--- install.sh
+++ install.sh
@@ -6,7 +6,7 @@ echo "Downloading taskfiles"
 wget "https://github.com/jotamontecino/env_scripts/archive/refs/tags/v$VERSION.tar.gz" -O /tmp/taskfile.tar.gz
 tar -zxf /tmp/taskfile.tar.gz -C /tmp/
 rm /tmp/taskfile.tar.gz
-mkdir $HOME/.taskfiles
+mkdir $HOME/.taskfile
 echo "Copy to Home folder"
 cp -a /tmp/env_scripts-$VERSION/.taskfile/. $HOME/.taskfile
 mv /tmp/env_scripts-$VERSION/Taskfile.yml $HOME/Taskfile.yml
